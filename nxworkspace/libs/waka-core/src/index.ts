0fca07a85d24497ecc0022eaad307f212c17a72e
havettaa <havettaa@gmail.com>
2022-02-04 14:19:22 +0100

    del

--- nxworkspace/libs/waka-core/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/waka-core.module';
--- nxworkspace/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: { config: './apps/wakapp/tailwind.config.js' },
-    autoprefixer: {},
-  },
-}
